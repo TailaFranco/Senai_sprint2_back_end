6824cd019f9f2c4812517414158b4aa0e1b1ed5c
Taila <taila.franco@aluno.ufabc.edu.br>
2021-05-07 21:01:29 -0300

    Projeto people

---
+++ M_Peoples/back/Senai.Peoples.WebApi/Senai.Peoples.WebApi/Controllers/FuncionarioController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Peoples.WebApi.Domains;
+using Senai.Peoples.WebApi.Interfaces;
+using Senai.Peoples.WebApi.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Peoples.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FuncionarioController : ControllerBase
+    {
+        private IFuncionarioRepository _funcionarioRepository { get; set; }
+
+        public FuncionarioController()
+        {
+            _funcionarioRepository = new FuncionarioRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<FuncionarioDomain> funcionarios = _funcionarioRepository.ListarTodos();
+            return Ok(funcionarios);
+        }
+
+        [HttpPost]
+        public IActionResult Post(FuncionarioDomain novoFuncionario)
+        {
+            _funcionarioRepository.Cadastrar(novoFuncionario);
+            return StatusCode(201);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _funcionarioRepository.Deletar(id);
+            return StatusCode(204);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            FuncionarioDomain funcionarioBuscado = _funcionarioRepository.BuscarPorId(id);
+            if (funcionarioBuscado == null)
+            {
+                return NotFound("Nenhum funcionario foi encontrado!");
+            }
+            return Ok(funcionarioBuscado);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutUrl(FuncionarioDomain funcionarioAtualizado, int id)
+        {
+            FuncionarioDomain funcionarioBuscado = _funcionarioRepository.BuscarPorId(id);
+            if (funcionarioBuscado != null)
+            {
+                try
+                {
+                    _funcionarioRepository.Atualizar(id, funcionarioAtualizado);
+                    return NoContent();
+                }
+                catch (Exception erro)
+                {
+
+                    return BadRequest(erro);
+                }
+            }
+            return NotFound
+                    (
+                    new
+                    {
+                        mensagem = "Funcionario não encontrado."
+                    }
+                    );
+        }
+
+    }
+}
---
+++ M_Peoples/back/Senai.Peoples.WebApi/Senai.Peoples.WebApi/Repositories/FuncionarioRepository.cs
@@ -0,0 +1,113 @@
+﻿using Senai.Peoples.WebApi.Domains;
+using Senai.Peoples.WebApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Peoples.WebApi.Repositories
+{
+    public class FuncionarioRepository : IFuncionarioRepository
+    {
+        private string stringConexao = "Data Source=DESKTOP-4MFDOSC\\SQLEXPRESS; initial catalog=M_Peoples_T_Peoples; user Id=sa; pwd=1297;";
+
+        public void Atualizar(int id, FuncionarioDomain funcionarioAtualizado)
+        {
+            using (SqlConnection con = new SqlConnection(stringConexao))
+            {
+                string queryUpdate = "UPDATE Funcionarios SET nome = @nome, sobrenome = @sobrenome WHERE idFuncionario = @Id";
+                using (SqlCommand cmd = new SqlCommand(queryUpdate, con))
+                {
+                    cmd.Parameters.AddWithValue("@nome", funcionarioAtualizado.nome);
+                    cmd.Parameters.AddWithValue("@sobrenome", funcionarioAtualizado.sobrenome);
+                    cmd.Parameters.AddWithValue("@Id", id);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public FuncionarioDomain BuscarPorId(int id)
+        {
+            using (SqlConnection con = new SqlConnection(stringConexao))
+            {
+                string querySelectById = "SELECT * FROM Funcionarios WHERE idFuncionario =@Id";
+                con.Open();
+                SqlDataReader rdr;
+                using (SqlCommand cmd = new SqlCommand(querySelectById, con))
+                {
+                    cmd.Parameters.AddWithValue("@Id", id);
+                    rdr = cmd.ExecuteReader();
+                    if (rdr.Read())
+                    {
+                        FuncionarioDomain funcionarioBuscado = new FuncionarioDomain
+                        {
+                            idFuncionario = Convert.ToInt32(rdr["idFuncionario"]),
+                            nome = rdr["nome"].ToString(),
+                            sobrenome = rdr["sobrenome"].ToString()
+                        };
+                        return funcionarioBuscado;
+                    }
+                    return null;
+                }
+            }
+        }
+
+        public void Cadastrar(FuncionarioDomain novoFuncionario)
+        {
+            using (SqlConnection con = new SqlConnection(stringConexao))
+            {
+                string queryInsert = "INSERT INTO Funcionarios(nome,sobrenome) VALUES (@nome, @sobrenome)";
+                using (SqlCommand cmd = new SqlCommand(queryInsert, con))
+                {
+                    cmd.Parameters.AddWithValue("@nome", novoFuncionario.nome);
+                    cmd.Parameters.AddWithValue("@sobrenome", novoFuncionario.sobrenome);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Deletar(int id)
+        {
+            using (SqlConnection con = new SqlConnection(stringConexao))
+            {
+                string queryDelete = "DELETE FROM Funcionarios WHERE idFuncionario = @Id";
+                using (SqlCommand cmd = new SqlCommand(queryDelete, con))
+                {
+                    cmd.Parameters.AddWithValue("@Id", id);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public List<FuncionarioDomain> ListarTodos()
+        {
+            List<FuncionarioDomain> funcionarios = new List<FuncionarioDomain>();
+            using (SqlConnection con = new SqlConnection(stringConexao))
+            {
+                string querySelectAll = "SELECT idFuncionario, nome, sobrenome FROM Funcionarios";
+                con.Open();
+                SqlDataReader rdr;
+
+                using (SqlCommand cmd = new SqlCommand(querySelectAll, con))
+                {
+                    rdr = cmd.ExecuteReader();
+                    while(rdr.Read())
+                    {
+                        FuncionarioDomain funcionario = new FuncionarioDomain
+                        {
+                            idFuncionario = Convert.ToInt32(rdr["idFuncionario"]),
+                            nome = rdr["nome"].ToString(),
+                            sobrenome = rdr["sobrenome"].ToString()
+                        };
+                        funcionarios.Add(funcionario);
+                    }
+                    return funcionarios;
+                }
+            }
+        }
+    }
+}
